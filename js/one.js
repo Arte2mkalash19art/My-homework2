40c237b0c21b77c15d6b78c19a8c536b5f9f2c4e
Arte2mkalash19art <129776037+Arte2mkalash19art@users.noreply.github.com>
2024-03-21 21:42:36 +0200

    Homeworks  20.03 - 27.03

---
+++ js/one.js
@@ -0,0 +1,23 @@
+const age = 16;
+console.log(age);
+
+const name = "Artem";
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString =
+  "Життя — це те, що з тобою відбувається, поки ти будуєш плани.";
+console.log(myString);
+
+let myNumber = 26;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+const resultName = prompt("Введіть своє ім'я");
+alert("Ласкаво просимо " + resultName);
+// просто
