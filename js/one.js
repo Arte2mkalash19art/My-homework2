9b25811074688109ad28b50b77518fad102d05e8
Arte2mkalash19art <129776037+Arte2mkalash19art@users.noreply.github.com>
2024-04-08 08:46:05 +0300

    Homework 10.04

--- js/one.js
+++ js/one.js
@@ -1,49 +1,22 @@
-//Перевести температуру з Цельсія у Фаренгейти за допомогою формули
-const celsius = Number(prompt("Введіть значення в цельсія"));
-alert("Результат: " + ((celsius * 9) / 5 + 32) + " по фаренгейту");
+const result = 5 + 5 + "5";
+console.log(result);
 
-//Обчислити кількість годин та хвилин у цьому місяці за допомогою оператора множення
-const inputDays = Number(prompt("Введіть кількість днів:"));
-const days = parseInt(inputDays);
+// Перевірка наявності символу @ у змінній email
+const email = "example@example.com";
+const containsAtSymbol = email.includes('@');
+const emailLength = email.length;
+console.log(`Чи містить email символ @: ${containsAtSymbol}`);
+console.log(`Загальна кількість символів у email: ${emailLength}`);
 
-if (!isNaN(days)) {
-  const hours = days * 24;
-  const minutes = days * 24 * 60;
+// Запис кожного слова в окрему змінну та їх поєднання
+const word1 = "My";
+const word2 = "name";
+const word3 = "is";
+let fullName = `${word1} ${word2} ${word3}`;
+fullName += ' Viktor';
+console.log(`Повне ім'я: ${fullName}`);
 
-  alert("Кількість в годин: " + hours + " Кількість в хвилинах: " + minutes);
-} else {
-  alert("Будь ласка, введіть коректне число днів.");
-}
-
-// Зменшення рівня здоров'я та енергії гравця
-let health = 100;
-let energy = 50;
-health -= 35;
-energy -= 13;
-console.log("Рівень здоров'я після атаки:", health);
-console.log("Рівень енергії після атаки:", energy);
-
-// Застосування знижки в розмірі 10%
-const purchaseAmount = 100;
-const discountedAmount = purchaseAmount * 0.9;
-console.log("Сума зі знижкою:", discountedAmount);
-
-// Використання методу Math.floor() для округлення числа до меншого
-const floatingNumber = 3.7;
-const floorNumber = Math.floor(floatingNumber);
-console.log("Округлене число:", floorNumber);
-
-// Використання методу parseFloat() для перетворення рядка у десяткове число
-const floatString = "5.5";
-const parsedFloat = parseFloat(floatString);
-console.log("Число після парсингу:", parsedFloat);
-
-// Використання методу parseInt() для перетворення рядка у ціле число
-const intString = "10";
-const parsedInt = parseInt(intString);
-console.log("Ціле число після парсингу:", parsedInt);
-
-// Використання методу Math.sqrt() для обчислення квадратного кореня числа
-const number = 16;
-const squareRoot = Math.sqrt(number);
-console.log("Квадратний корінь числа:", squareRoot);
+// Виведення повідомлення про оплату через alert
+const userName = "Олександро";
+const payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
