bc04bf7aa3a60369c176800203aeb54ef648fe79
Arte2mkalash19art <129776037+Arte2mkalash19art@users.noreply.github.com>
2024-05-07 15:53:13 +0300

    08.05

--- js/one.js
+++ js/one.js
@@ -1,49 +1,44 @@
-//Виведення на екран чисел від 1 до 10 за допомогою циклу while:
-/* let i = 1;
-while (i <= 10) {
-    console.log(i);
-    i++;
-} */
+// 1. Створення масиву з трьох чисел і зміна значення другого елемента на 10.
+let numbersArray = [1, 2, 3];
+numbersArray[1] = 10;
+console.table(numbersArray);
 
-//Виведення на екран всіх парних чисел від 2 до 20 за допомогою циклу for, пропускаючи парні числа за допомогою continue:
-/* for (let i = 2; i <= 20; i += 2) {
-  if (i % 2 === 0) {
-      console.log(i);
-      continue;
-  }
-} */
+// 2. Створення масиву із трьох рядків і додавання ще одного рядка.
+let stringsArray = ["one", "two", "three"];
+stringsArray.push("four");
+console.table(stringsArray);
 
-//Виведення на екран таблиці множення числа 7 за допомогою циклу for:
-/* const number = 7;
-for (let i = 1; i <= 10; i++) {
-    console.log(`${number} * ${i} = ${number * i}`);
-} */
-
-//Виведення на екран всіх чисел від 1 до 10, крім числа 7, і закінчення циклу при зустрічі числа 7 за допомогою оператора break:
-/* for (let i = 1; i <= 10; i++) {
-  if (i === 7) {
-      break;
-  }
-  console.log(i);
-} */
-
-//Створення скрипта, який виводить на екран всі числа менші за n, а якщо зустрічається число, що більше або дорівнює n, цикл повинен бути закінчений за допомогою break:
-/* const n = 15;
-for (let i = 1; i < n; i++) {
-    if (i >= n) {
-        break;
-    }
-    console.log(i);
-} */
-
-//Виведення на екран всіх чисел від 1 до 20, крім чисел, кратних 3, за допомогою оператора continue:
-/* let i = 1;
-while (i <= 20) {
-    if (i % 3 === 0) {
-        i++;
-        continue;
-    }
-    console.log(i);
-    i++;
+// 3. Скрипт, що повертає суму всіх чисел в масиві.
+let array = [5, 1, 4];
+let sum = 0;
+for (let i = 0; i < array.length; i += 1) {
+  sum += array[i];
 }
- */
+console.log(`Сума = ${sum}`);
+
+// 4. Створення масиву з 5 чисел і виведення на екран всіх елементів масиву за допомогою циклу for.
+let numbersArray2 = [1, 2, 3, 4, 5];
+for (let i = 0; i < numbersArray2.length; i += 1) {
+  console.log(numbersArray2[i]);
+}
+
+// 5. Створення масиву із 5 рядків і виведення на екран кожного рядка, який містить більше 5 символів.
+let stringsArray2 = ["one", "two", "three", "four", "five"];
+for (let i = 0; i < stringsArray2.length; i++) {
+  if (stringsArray2[i].length > 5) {
+    console.log(stringsArray2[i]);
+  }
+}
+
+// 6. Створення масиву з 10 чисел і знаходження максимального значення.
+let numbersArray3 = [5, 8, 12, 3, 6, 10, 7, 20, 15, 1];
+let maxNumber = Math.max(...numbersArray3);
+console.log("Максимальне значення: " + maxNumber);
+
+// 7. Створення масиву з 10 чисел і виведення всіх парних чисел.
+let numbersArray4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < numbersArray4.length; i++) {
+  if (numbersArray4[i] % 2 === 0) {
+    console.log(numbersArray4[i]);
+  }
+}
